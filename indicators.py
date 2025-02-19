0a39fc32c9dab480735675ffc6a233cc75620527
AllinaChan <zchen5143@bths.edu>
2025-02-18 23:12:17 -0500

    added request

--- indicators.py
+++ indicators.py
@@ -8,7 +8,7 @@ from scipy.stats import gaussian_kde
 from scipy.ndimage import gaussian_filter1d
 from sklearn.mixture import GaussianMixture
 import datetime as datetime
-
+import requests
 
 
 # ---------------------------------------------------------------------------
@@ -22,7 +22,7 @@ def get_alpha_data(symbol, period="1mo"):
     """
     alpha_key = st.secrets['ALPHAKEY']
     alphaURL = st.secrets['ALPHAURL']
-    url = f"{alphaURL}?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={alpha_key}"
+    url = f"{alphaURL}?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=compact&apikey={alpha_key}"
     response = requests.get(url)
     if response.status_code != 200:
         st.warning(f"Error fetching data for {symbol} from Alpha Vantage.")

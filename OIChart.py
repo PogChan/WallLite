0830b6f3a2fd1f2636d71dfc784dd3ea3853e995
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2025-02-28 09:50:30 -0500

    Spx to ^spx

--- OIChart.py
+++ OIChart.py
@@ -230,6 +230,8 @@ def plotChartOI(symbol, data, exp_date, top_n=5):
 # Function: Options Volume Check (Aggregate across expirations)
 # ---------------------------------------------------------------------------
 def pc_check(symbol, data, top_n=5):
+    if symbol = "SPX":
+        symbol ="^SPX"
     df = getHistoricalOHLC(symbol)
     if df.empty:
         st.warning(f"No price data for {symbol}.")

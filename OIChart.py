21e75c313b11b155f2902780f047d2cb9347a394
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2025-03-02 18:39:50 -0500

    slgiht mistypo

--- OIChart.py
+++ OIChart.py
@@ -289,7 +289,7 @@ def pc_check(symbol, data, top_n=5):
         if exp_date == today_date and now.hour >= 16:
             continue
         #if 2025-03-21W > 2025-03-21 then we remove the W and is it equal then we skip it. if its not then we go next.
-        if exp_date > get_next_opex() and exp_data.replace('W', '') > get_next_opex():
+        if exp_date > get_next_opex() and exp_date.replace('W', '') > get_next_opex():
             break
 
 
